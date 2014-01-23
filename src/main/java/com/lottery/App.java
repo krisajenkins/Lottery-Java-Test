f3ae7c0a869ef347ee07714a43c971dee6e17786
Kris Jenkins <krisajenkins@gmail.com>
2014-01-23 20:03:41 +0000

    Parsing the command-line arguments.

--- src/main/java/com/lottery/App.java
+++ src/main/java/com/lottery/App.java
@@ -1,8 +1,26 @@
 package com.lottery;
 
-public class App {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class App {
+	private static DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/mm/yyyy");
+	
     public static void main(String [] args) {
-        // Code here
+    	// Parse out the command line arguments.
+    	DateTime endDate = formatter.parseDateTime(args[0]);
+    	
+    	List<Integer> selection = new ArrayList<Integer>();
+    	for (int i = 1; i < args.length; i++) {
+    		Integer number = Integer.parseInt(args[i]);
+    		selection.add(number);
+    	}
+    	
+    	System.out.format("Date was: %s\n", endDate);
+    	System.out.format("Selection was: %s\n", selection);
     }
 }
