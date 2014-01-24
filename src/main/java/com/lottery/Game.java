e7b1350e83acb97359b2a690fe72854d0120b5ce
Kris Jenkins <krisajenkins@gmail.com>
2014-01-24 11:37:15 +0000

    Handling the leap-day prize-multiplier rules.

--- src/main/java/com/lottery/Game.java
+++ src/main/java/com/lottery/Game.java
@@ -7,17 +7,42 @@ import java.math.BigInteger;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.joda.time.DateTime;
+
 public class Game {
-	public static BigInteger valueResult(
-		Set<Integer> winningNumbers,
-		Set<Integer> chosenNumbers
-	) {
-		Set<Integer> correctlyChosenNumbers = new HashSet<Integer>(winningNumbers);
+	public static BigInteger calculatePrize(DateTime drawDate,
+			Set<Integer> winningNumbers, Set<Integer> chosenNumbers) {
+
+		BigInteger rawPrize = calculateRawPrize(drawDate, winningNumbers, chosenNumbers);
+		BigInteger multiplier = calculateMultiplier(drawDate);
+
+		return rawPrize.multiply(multiplier);
+	}
+
+	private static BigInteger calculateMultiplier(DateTime drawDate) {
+		// "There are a few special cases. During the month of February on a
+		// leap year, all winnings are doubled and if a draw falls on Monday the
+		// 29th of February they are tripled."
+		if (drawDate.monthOfYear().isLeap()) {
+			if (drawDate.getDayOfMonth() == 29) {
+				return BigInteger.valueOf(3);
+			}
+			
+			return BigInteger.valueOf(2);
+		}
+
+		return BigInteger.ONE;
+	}
+
+	private static BigInteger calculateRawPrize(DateTime drawDate,
+			Set<Integer> winningNumbers, Set<Integer> chosenNumbers) {
+		Set<Integer> correctlyChosenNumbers = new HashSet<Integer>(
+				winningNumbers);
 		correctlyChosenNumbers.retainAll(chosenNumbers);
-		
+
 		Set<Integer> missedNumbers = new HashSet<Integer>(winningNumbers);
 		missedNumbers.removeAll(chosenNumbers);
-		
+
 		switch (correctlyChosenNumbers.size()) {
 		case 0:
 		case 1:
@@ -26,12 +51,13 @@ public class Game {
 		case 3:
 		case 4:
 		case 5:
-			return BigInteger.valueOf(1000 * correctlyChosenNumbers.size()).add(productOf(missedNumbers)); 
+			return BigInteger.valueOf(1000 * correctlyChosenNumbers.size())
+					.add(productOf(missedNumbers));
 		case 6:
-			return sumOf(winningNumbers).multiply(BigInteger.valueOf(10000)); 
+			return sumOf(winningNumbers).multiply(BigInteger.valueOf(10000));
 		default:
 			// TODO Testme.
 			throw new IllegalStateException();
-		}		
+		}
 	}
 }
--- src/test/java/com/lottery/GameTest.java
+++ src/test/java/com/lottery/GameTest.java
@@ -1,5 +1,7 @@
 package com.lottery;
 
+import static com.lottery.DateTimeUtils.normaliseToMonday;
+import static com.lottery.DateTimeUtils.parseDateTime;
 import static com.lottery.SetUtils.setOf;
 import static com.lottery.SetUtils.sumOf;
 import static org.junit.Assert.assertEquals;
@@ -7,19 +9,63 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigInteger;
 import java.util.Set;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class GameTest {
 
 	@Test
-	public void testValueResult() {
-		Set<Integer> winningNumbers = setOf(1, 2, 3, 4, 5, 6);
+	public void testValueResultSimple() {
+		DateTime drawDate = normaliseToMonday(parseDateTime("01/01/2000"));
+		Integer multiplier;
 
-		assertEquals(sumOf(winningNumbers), Game.valueResult(winningNumbers, setOf(11, 12, 13, 14, 15, 16)));
-		assertEquals(sumOf(winningNumbers), Game.valueResult(winningNumbers, setOf(1, 12, 13, 14, 15, 16)));
-		assertEquals(sumOf(winningNumbers), Game.valueResult(winningNumbers, setOf(1, 2, 13, 14, 15, 16)));
-		assertEquals(BigInteger.valueOf((4 * 1000) + (5 * 6)), Game.valueResult(winningNumbers, setOf(1, 2, 3, 4, 15, 16)));
-		assertEquals(BigInteger.valueOf((5 * 1000) + 6), Game.valueResult(winningNumbers, setOf(1, 2, 3, 4, 5, 16)));
-		assertEquals(BigInteger.valueOf(10000 * (1 + 2 + 3 + 4 + 5 + 6)), Game.valueResult(winningNumbers, setOf(1, 2, 3, 4, 5, 6)));
+		while (drawDate.getYear() < 2100) {
+			if (drawDate.monthOfYear().isLeap()) {
+				if (drawDate.getDayOfMonth() == 29) {
+					multiplier = 3;
+				} else {
+					multiplier = 2;
+				}
+			} else {
+				multiplier = 1;
+			}
+
+			Set<Integer> winningNumbers = setOf(1, 2, 3, 4, 5, 6);
+
+			// Losing rule.
+			checkSingleGame(drawDate, sumOf(winningNumbers), multiplier,
+					winningNumbers, 11, 12, 13, 14, 15, 16);
+			checkSingleGame(drawDate, sumOf(winningNumbers), multiplier,
+					winningNumbers, 1, 12, 13, 14, 15, 16);
+			checkSingleGame(drawDate, sumOf(winningNumbers), multiplier,
+					winningNumbers, 1, 2, 13, 14, 15, 16);
+
+			// Middle-prize rule.
+			checkSingleGame(drawDate, (4 * 1000) + (5 * 6), multiplier,
+					winningNumbers, 1, 2, 3, 4, 15, 16);
+			checkSingleGame(drawDate, (5 * 1000) + 6, multiplier,
+					winningNumbers, 1, 2, 3, 4, 5, 16);
+
+			// Winning rule.
+			checkSingleGame(drawDate, 10000 * (1 + 2 + 3 + 4 + 5 + 6),
+					multiplier, winningNumbers, 1, 2, 3, 4, 5, 6);
+
+			drawDate = drawDate.plusWeeks(1);
+		}
+	}
+
+	private void checkSingleGame(DateTime drawDate, BigInteger expectedPrize,
+			Integer multiplier, Set<Integer> winningNumbers,
+			Integer... chosenNumbers) {
+		assertEquals(
+				expectedPrize.multiply(BigInteger.valueOf(multiplier)),
+				Game.calculatePrize(drawDate, winningNumbers, setOf(chosenNumbers)));
+	}
+
+	private void checkSingleGame(DateTime drawDate, Integer expectedPrize,
+			Integer multiplier, Set<Integer> winningNumbers,
+			Integer... chosenNumbers) {
+		checkSingleGame(drawDate, BigInteger.valueOf(expectedPrize),
+				multiplier, winningNumbers, chosenNumbers);
 	}
 }
