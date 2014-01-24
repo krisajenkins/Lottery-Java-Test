58b4e6f09f30820272b166ad945c3587af937682
Kris Jenkins <krisajenkins@gmail.com>
2014-01-24 11:38:35 +0000

    Minor code tidyup.

--- src/main/java/com/lottery/App.java
+++ src/main/java/com/lottery/App.java
@@ -1,26 +1,25 @@
 package com.lottery;
 
-import java.util.ArrayList;
-import java.util.List;
+import static com.lottery.DateTimeUtils.parseDateTime;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 public class App {
-	private static DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/mm/yyyy");
-	
-    public static void main(String [] args) {
-    	// Parse out the command line arguments.
-    	DateTime endDate = formatter.parseDateTime(args[0]);
-    	
-    	List<Integer> selection = new ArrayList<Integer>();
-    	for (int i = 1; i < args.length; i++) {
-    		Integer number = Integer.parseInt(args[i]);
-    		selection.add(number);
-    	}
-    	
-    	System.out.format("Date was: %s\n", endDate);
-    	System.out.format("Selection was: %s\n", selection);
-    }
+	public static void main(String[] args) {
+		// Parse out the command line arguments. Perhaps we should spin this out
+		// into a GameConfig class, but that feels like over-engineering.
+		DateTime endDate = parseDateTime(args[0]);
+
+		Set<Integer> selection = new HashSet<Integer>();
+		for (int i = 1; i < args.length; i++) {
+			Integer number = Integer.parseInt(args[i]);
+			selection.add(number);
+		}
+
+		System.out.format("Date was: %s\n", endDate);
+		System.out.format("Selection was: %s\n", selection);
+	}
 }
