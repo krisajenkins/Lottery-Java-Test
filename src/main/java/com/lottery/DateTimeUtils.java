bf759b2b76126798cf4161f24bd09f60c7b32a07
Kris Jenkins <krisajenkins@gmail.com>
2014-01-24 09:52:30 +0000

    Refactoring DateTime parsing.
    Let's make the preferred date format a system-wide thing.

--- src/main/java/com/lottery/DateTimeUtils.java
+++ src/main/java/com/lottery/DateTimeUtils.java
@@ -3,8 +3,16 @@ package com.lottery;
 import static org.joda.time.DateTimeConstants.MONDAY;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class DateTimeUtils {
+	private static DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/mm/yyyy");
+
+	public static DateTime parseDateTime(String string) {
+		return formatter.parseDateTime(string);
+	}
+
 	public static DateTime normaliseToMonday(DateTime date) {
 		return date.dayOfWeek().setCopy(MONDAY);
 	}
