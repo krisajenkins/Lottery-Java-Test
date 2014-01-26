d0ede3bb75363f0f3703a972423717ed235785e6
Kris Jenkins <krisajenkins@gmail.com>
2014-01-26 13:24:18 +0000

    Improving the DrawResult tests.

--- src/test/java/com/lottery/DrawResultTest.java
+++ src/test/java/com/lottery/DrawResultTest.java
@@ -11,8 +11,11 @@ import org.junit.Test;
 public class DrawResultTest {
 	@Test
 	public void testToString() {
-		assertEquals("01/02/2003; 1, 2, 3, 4, 5, 6; 12345", new DrawResult(
-				parse("01/02/2003"), Arrays.asList(1, 2, 3, 4, 5, 6),
+		assertEquals("10/04/2003; 1, 2, 3, 4, 5, 6; 12345", new DrawResult(
+				parse("10/04/2003"), Arrays.asList(1, 2, 3, 4, 5, 6),
 				BigInteger.valueOf(12345)).toString());
+		assertEquals("29/02/2008; 60, 59, 58, 57, 56, 55; 54321", new DrawResult(
+				parse("29/02/2008"), Arrays.asList(60, 59, 58, 57, 56, 55),
+				BigInteger.valueOf(54321)).toString());
 	}
 }
